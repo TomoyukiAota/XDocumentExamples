be5513383eee284868e07ac0915b8ed55d102864
Tomoyuki Aota <tomoyukia@gmail.com>
2017-07-06 18:54:16 +0900

    Add XDocumentExamples

---
+++ XDocumentExamples/XDocumentExamples/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml.Linq;
+
+namespace XDocumentExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //XDocumentGeneration();
+            XDocumentManipulation();
+        }
+
+        private static void XDocumentManipulation()
+        {
+            var xDocument = new XDocument();
+            var root = new XElement("Root");
+            var a = new XElement("a");
+            var b = new XElement("b", new XText("value"));
+
+            root.Add(a, b);
+            xDocument.Add(root);
+            XDocumentUtilities.DisplayInConsole(xDocument, "Add Element");
+            XDocumentUtilities.SaveAsXmlFile(xDocument, "Add Element");
+        }
+
+        private static void XDocumentGeneration()
+        {
+            var xDocument = new XDocument(
+                new XDeclaration(version: "1.0", encoding: "utf-8", standalone: null),
+                new XElement("ElementA",
+                    new XAttribute("Attribute", "AttributeValue"),
+                    new XElement("ElementB",
+                        new XText("ElementB_Text"),
+                        new XCData("This is CData section not recognized as XML document.")
+                    )));
+
+            XDocumentUtilities.DisplayInConsole(xDocument, "XDocument-Generation");
+            XDocumentUtilities.SaveAsXmlFile(xDocument, "XDocument-Generation");
+        }
+    }
+}
---
+++ XDocumentExamples/XDocumentExamples/XDocumentUtilities.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XDocumentExamples
+{
+    /// <summary>
+    /// Utility class for <see cref="XDocument"/>.
+    /// </summary>
+    /// <remarks>
+    /// The content of <see cref="XDocument"/> object must be a valid XML document.
+    /// e.g. the XML document must contain a single root node.
+    /// </remarks>
+    public class XDocumentUtilities
+    {
+        /// <summary>
+        /// Display <see cref="XDocument"/> object in <see cref="Console"/> including XML declaration.
+        /// </summary>
+        public static void DisplayInConsole(XDocument xDocument, string title = "")
+        {
+            if (!string.IsNullOrWhiteSpace(title))
+            {
+                Console.WriteLine($"Title: {title}");
+            }
+
+            using (var writer = new Utf8StringWriter())
+            {
+                xDocument.Save(writer);
+                Console.WriteLine(writer);
+            }
+
+            Console.WriteLine("---------------------------------");
+        }
+
+        private static readonly string SaveDirectory = @"../../../GeneratedXmlFiles";
+
+        /// <summary>
+        /// Save <see cref="XDocument"/> object as a XML file in the directory specified in <see cref="SaveDirectory"/>.
+        /// </summary>
+        public static void SaveAsXmlFile(XDocument xDocument, string title)
+        {
+            var now = DateTime.UtcNow.ToString("yyyyMMdd-HHmmss");
+            var path = $@"{SaveDirectory}/{now}_{title}.xml";
+            xDocument.Save(path);
+        }
+    }
+}
